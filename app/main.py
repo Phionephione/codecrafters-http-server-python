24c870a152e8e84631e4e31f0be56e8a14be4f11
Ahmed Ezzeldin <a.ezzeldin@me.com>
2023-10-26 16:57:50 +0300

    add text/plain response

--- app/main.py
+++ app/main.py
@@ -12,10 +12,16 @@ def main():
     http_method, path, http_version = request[0].split(" ")
 
     if path == "/":
-        response = "HTTP/1.1 200 OK\r\n\r\n".encode()
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+    elif path.startswith('/echo'):
+        string = path.split("/")[2]
+        length = len(string)
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{string}\r\n"
     else:
-        response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
-    conn.send(response)
+        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+
+    conn.send(response.encode())
+
 
 
 if __name__ == "__main__":
