d088c240ebcd33392b2cb6b8355b1ba417f3a1f3
Ahmed Ezzeldin <a.ezzeldin@me.com>
2023-10-27 16:11:43 +0200

    Fix

--- app/main.py
+++ app/main.py
@@ -8,15 +8,18 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
     request = conn.recv(4096)
-    request = request.decode().split("\r\n")
-    http_method, path, http_version = request[0].split(" ")
+    request = request.decode().splitlines()
+    _, path, _ = request[0].split(" ")
 
     if path == "/":
         response = "HTTP/1.1 200 OK\r\n\r\n"
     elif path.startswith('/echo'):
-        string = path.split("/")[2]
-        length = len(string)
-        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{string}\r\n"
+        content = path.split("/echo/")[1]
+        response = ("HTTP/1.1 200 OK\r\n"
+                    "Content-Type: text/plain\r\n"
+                    f"Content-Length: {len(content)}\r\n"
+                    "\r\n"
+                    f"{content}\r\n")
     else:
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
 
