2d4fa684b0fb0cc18d691447fee8c761d40a6f07
Ahmed Ezzeldin <a.ezzeldin@me.com>
2023-10-28 15:38:59 +0200

    Add support for file streaming

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
+import sys
 import socket
 import threading
 
+
+HOST = "localhost"
+PORT = 4221
+FILES_DIR = ""
 
 HTTP_STATUS_OK = "HTTP/1.1 200 OK\r\n"
 HTTP_STATUS_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n"
 
 
-def handle_client(conn, addr):
-    request = conn.recv(4096)
-    request = request.decode().splitlines()
-
+def get_response(request, files=None):
     _, path, _ = request[0].split(" ")
 
     if path == "/":
@@ -28,15 +30,48 @@ def handle_client(conn, addr):
                     f"Content-Length: {len(content)}\r\n"
                     "\r\n"
                     f"{content}\r\n")
+    elif path.startswith('/files'):
+        file_name = path.split("/files/")[1]
+        file_content = handle_files(file_name)
+
+        if file_content:
+            response = (HTTP_STATUS_OK +
+                        "Content-Type: application/octet-stream\r\n"
+                        f"Content-Length: {len(file_content)}\r\n"
+                        "\r\n"
+                        f"{file_content}\r\n")
+        else:
+            response = HTTP_STATUS_NOT_FOUND + "\r\n"
+
     else:
         response = HTTP_STATUS_NOT_FOUND + "\r\n"
+
+    return response
+
+
+def handle_files(file_name):
+    try:
+        with open(f"{FILES_DIR}{file_name}", "r") as f:
+            file = f.read()
+    except FileNotFoundError:
+        file = None
+    return file
+
+
+def handle_client(conn, addr):
+    request = conn.recv(4096)
+    request = request.decode().splitlines()
+    response = get_response(request)
 
     conn.send(response.encode())
     conn.close()
 
 
-def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+def main(args):
+    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
+    if args:
+        global FILES_DIR
+        FILES_DIR = args[1]
 
     while True:
         conn, addr = server_socket.accept() # wait for client
@@ -48,4 +83,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
+
